c8e911af8ddb6be27c35a74c2defe66a0754c531
Lara Jane H. <47236555+yodabread912@users.noreply.github.com>
2026-02-13 15:44:20 +0800

    Update Container.tsx

---
+++ Container.tsx
@@ -0,0 +1,4 @@
+import React from 'react'
+export default function Container({ children, className = '' }: { children: React.ReactNode; className?: string }) {
+  return <div className={`container ${className}`}>{children}</div>
+}
